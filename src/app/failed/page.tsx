641824d4f4142af3b08e48f5f432a15b1447c386
Jaewon Chung <jaewonchung97@gmail.com>
2024-08-15 17:25:51 +0900

    updated

--- src/app/failed/page.tsx
+++ src/app/failed/page.tsx
@@ -5,7 +5,9 @@ export default function Page() {
         <div className={`flex flex-col w-full h-screen items-center justify-center space-y-4`}>
             <h1 className={`text-3xl font-bold text-red-500`}>실패</h1>
             <Link href={'/'}>
-                <span>재시도</span>
+                <div className={`px-2 py-1 rounded-md flex items-center justify-center hover:bg-zinc-200 text-zinc-500 hover:text-zinc-950 transition-colors duration-500`}>
+                    <span>재시도</span>
+                </div>
             </Link>
         </div>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,9 @@ export default function Home() {
         <div className={`w-full h-screen flex flex-col items-center justify-center space-y-5`}>
             <h1 className={`text-3xl font-medium`}>방탈출 하기</h1>
             <Link href={'/failed'}>
-                <FaArrowRight className={`font-light cursor-pointer`}/>
+                <div className={`flex w-10 h-10 transition-colors duration-500 border-2 border-blue-500 hover:bg-blue-500 text-blue-500 hover:text-white justify-center items-center rounded-full`}>
+                    <FaArrowRight className={`font-light cursor-pointer`}/>
+                </div>
             </Link>
         </div>
     );
